81028a2b3e1b5f2af0db63dc5ba2cea1ff6a3384
Jaeyoun Nam <siisee111@gmail.com>
2020-01-10 00:23:36 +0900

    Update index page

--- Blog/gatsby-config.js
+++ Blog/gatsby-config.js
@@ -5,12 +5,13 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/`,
       },
     },
     `gatsby-transformer-sharp`,
